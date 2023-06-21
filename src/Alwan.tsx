b620f1a8fbf7005999787244e6d8e09052616c36
Sefiane Chouaib <sefian.chouaib@gmail.com>
2023-06-21 22:45:27 +0100

    add color state

--- src/Alwan.tsx
+++ src/Alwan.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './assets/alwan.scss';
 import Container from './components/Container';
 import Inputs from './components/Inputs';
@@ -5,8 +6,24 @@ import Palette from './components/Palette';
 import Sliders from './components/Sliders';
 import Swatches from './components/Swatches';
 import Utility from './components/Utility';
+import type { colorState } from './types';
 
 const Alwan = () => {
+    const [color, setColor] = useState<colorState>({
+        h: 0,
+        s: 0,
+        l: 0,
+
+        r: 0,
+        g: 0,
+        b: 0,
+        a: 1,
+
+        rgb: '',
+        hsl: '',
+        hex: '',
+    });
+
     return (
         <>
             <button type='button' className='alwan__button alwan__preset-button'></button>
---
+++ src/types.ts
@@ -0,0 +1,25 @@
+export interface RGB {
+    r: number;
+    g: number;
+    b: number;
+}
+
+export interface RGBA extends RGB {
+    a: number;
+}
+
+export interface HSL {
+    h: number;
+    s: number;
+    l: number;
+}
+
+export interface HSLA extends HSL {
+    a: number;
+}
+
+export interface colorState extends RGBA, HSL {
+    rgb: string;
+    hsl: string;
+    hex: string;
+}
