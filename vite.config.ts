bffbcf19c97c905518e4ccf48391be7644decc00
Dineshmohan Rajaveeran <dinesh.applications@gmail.com>
2025-02-04 10:57:10 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -37,6 +37,7 @@ export default defineConfig(({ mode }) => ({
                     },
                     globals: {
                         react: 'React'
+                        'react-dom': 'ReactDOM'
                     },
                 },
             },
