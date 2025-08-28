cba6f88ea9041449c49f89f7808331efea6441f9
Sefiane Chouaib <sefian.chouaib@gmail.com>
2025-08-28 22:25:33 +0100

    refactor: replace previous React implementation with a React component wrapping Alwan library

---
+++ alwan.tsx
@@ -0,0 +1,45 @@
+import AlwanInstance from 'alwan';
+import "alwan/dist/css/alwan.min.css";
+import { alwanEventHandler, alwanOptions } from 'alwan/dist/js/types/src/types';
+import { ReactElement, cloneElement, useEffect, useRef } from 'react';
+
+interface AlwanProps {
+    options?: alwanOptions;
+    onChange?: alwanEventHandler;
+    onColor?: alwanEventHandler;
+    onOpen?: alwanEventHandler;
+    onClose?: alwanEventHandler;
+    refEl?: ReactElement;
+}
+
+const Alwan = ({
+    refEl,
+    options,
+    onChange,
+    onColor,
+    onOpen,
+    onClose
+}: AlwanProps) => {
+    const alwanRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (!alwanRef.current) {
+            return;
+        }
+
+        const alwan = new AlwanInstance(alwanRef.current, options);
+        onChange && alwan.on('change', onChange);
+        onColor && alwan.on('color', onColor);
+        onOpen && alwan.on('open', onOpen);
+        onClose && alwan.on('close', onClose);
+
+        return () => {
+            alwan.destroy();
+        }
+
+    }, [options, onChange, onColor, onOpen, onClose]);
+
+    return refEl ? cloneElement(refEl, { ref: alwanRef }) : <div ref={alwanRef}></div>;
+}
+
+export default Alwan;
