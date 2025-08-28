5355a811eaf4a9113807ea61c88225cd78a8d4bf
Sefiane Chouaib <sefian.chouaib@gmail.com>
2025-08-28 22:26:27 +0100

    chore: update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,28 @@
-import { defineConfig } from 'vite';
-import { terser } from 'rollup-plugin-terser';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import dts from 'vite-plugin-dts';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-    ...(mode === 'production' ? { define: { 'process.env.NODE_ENV': '"production"' } } : {}),
+    ...(mode === "production"
+        ? { define: { "process.env.NODE_ENV": '"production"' } }
+        : {}),
     ...{
         server: {
-            open: './test/index.html',
+            open: "./dev/index.html",
         },
         plugins: [react(), dts()],
         build: {
-            minify: 'terser',
             lib: {
-                entry: path.resolve(__dirname, 'src/index.ts'),
-                name: 'alwan',
-                fileName(format) {
-                    const fileName = 'react-alwan.js';
-                    if (format === 'umd') {
-                        return 'umd/' + fileName;
-                    }
-                    return fileName;
-                },
+                entry: "alwan",
+                formats: ["es", "cjs"],
+                fileName: (format) => `react-alwan.${format}.js`,
             },
-            sourcemap: true,
             emptyOutDir: true,
             rollupOptions: {
-                plugins: [terser()],
-                external: ['react', 'react-dom'],
-
+                external: ["react", "react-dom"],
                 output: {
-                    assetFileNames() {
-                        return 'alwan.css';
-                    },
-                    globals: {
-                        react: 'React',
-                        'react-dom': 'ReactDOM',
-                    },
+                    assetFileNames: "alwan.css",
                 },
             },
         },
